7e50b7e50ab1e5bbfe039cff1fbe9f92b98ce84b
Tan Shun Yuan <lowkim50@gmail.com>
2019-06-01 17:25:30 +0800

    Broken Form

--- lib/form.dart
+++ lib/form.dart
@@ -1,30 +1,92 @@
-
 import 'package:flutter/material.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class FormPage extends StatefulWidget {
-  
   FormPage({Key key}) : super(key: key);
 
   @override
   _FormState createState() => _FormState();
 }
 
+// _saveTextFields(url, description) async {
+//   final SharedPreferences prefs = await SharedPreferences.getInstance();
+//    prefs.setString('url', url);
+//    prefs.setString('description', description);
+// }
+
 class _FormState extends State<FormPage> {
+  // Create a text controller. We will use iturl to retrieve the current value
+  // of the TextField!
+  dynamic urlController = TextEditingController();
+  dynamic descriptionController = TextEditingController();
+  @override
+  void dispose() {
+    // Clean up the controller when the Widget is disposed
+    urlController.dispose();
+    descriptionController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Form Page"),
-      ),
-      body: Center(
-        child: Text("This is a form page"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print ("action button pressed");
-        },
-        child: Icon(Icons.plus_one),
-      )
-    );
+        appBar: AppBar(
+          title: Text("New Entry"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Container(
+              // decoration: BoxDecoration(color: Colors.red),
+              alignment: Alignment(1.0, 0.0),
+              child: Column(children: <Widget>[
+                Text('Url'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: urlController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Url'),
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+                // decoration: BoxDecoration(color: Colors.grey),
+                // height: MediaQuery.of(context).size.height * 0.3,
+                // width: MediaQuery.of(context).size.width * 0.9,
+                child: Column(
+              children: <Widget>[
+                Text('Description'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: descriptionController,
+                    maxLines: 4,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: 'Description'),
+                  ),
+                ),
+              ],
+            )),
+            FlatButton(
+              // onPressed: () {
+                // _saveTextFields(urlController.text, descriptionController.text);
+              // },
+              onPressed: (){},
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text('Save'),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("action button pressed");
+          },
+          child: Icon(Icons.plus_one),
+        ));
   }
 }
+
+// Define a Custom Form Widget
--- lib/user.dart
+++ lib/user.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import './form.dart';
 
 class User extends StatefulWidget {
-  
   User({Key key}) : super(key: key);
 
   @override
@@ -13,36 +12,32 @@ class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("User Page"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text("This is a user page"),
-          ),
-          FlatButton(
-            onPressed: () {
-              Navigator.push(
+        appBar: AppBar(
+          title: Text("User Page"),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(
+              child: Text("This is a user page"),
+            ),
+            FlatButton(
+              onPressed: () {},
+              color: Colors.red,
+              textColor: Colors.white,
+              child: Text('Go to form page'),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
                 context,
                 PageRouteBuilder(
-                  pageBuilder: (context, anim1, anim2) => FormPage()
-                )
-              );
-            },
-            color: Colors.red,
-            textColor: Colors.white,
-            child: Text('Go to form page'),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print ("action button pressed");
-        },
-        child: Icon(Icons.plus_one),
-      )
-    );
+                    pageBuilder: (context, anim1, anim2) => FormPage()));
+            print("action button pressed");
+          },
+          child: Icon(Icons.plus_one),
+        ));
   }
 }
